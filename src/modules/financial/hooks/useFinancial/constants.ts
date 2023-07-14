bf06a9b5cea373407127ecd99bf9bd7694f2cfdc
Amauri Dias <athlon.amauri@gmail.com>
2023-07-14 16:33:54 -0300

    chore: added validation max code 999

---
+++ src/modules/financial/hooks/useFinancial/constants.ts
@@ -0,0 +1,1 @@
+export const MAX_CODE_NUMBER = 999 as const;
--- src/modules/financial/hooks/useFinancial/utils.ts
+++ src/modules/financial/hooks/useFinancial/utils.ts
@@ -1,15 +1,16 @@
+import { MAX_CODE_NUMBER } from "./constants";
 import { FinancialRecord } from "../../entities/FinancialRecord";
 
-export const financialRecordsValidations = (
-  storedRecords: FinancialRecord[]
-) => {
-  /*
+/*
     foi criado esses 2 maps pois a primeira versão dessa função
     utilizava 2 finds, um para  validação de aceita lançamentos e
     outro para validar código já existente e isso tinha uma 
     complexidade linear O(n) a implementação dos maps permite que
     faça a busca em complexidade O(1) tempo constante
   */
+export const financialRecordsValidations = (
+  storedRecords: FinancialRecord[]
+) => {
   const recordsById = new Map(
     storedRecords.map((record) => [record.id, record])
   );
@@ -19,13 +20,26 @@ export const financialRecordsValidations = (
 
   return {
     add: (newRecord: FinancialRecord) => {
+      /*
+        O maior código possível é “999” independente do nível que você está.
+       */
+      const isValid = newRecord.code.split(".").every((part) => {
+        const number = Number(part);
+        return !isNaN(number) && number >= 1 && number <= MAX_CODE_NUMBER;
+      });
+
+      if (!isValid) {
+        throw new Error(
+          `O código deve ser composto por números entre 1 e ${MAX_CODE_NUMBER} separados por pontos`
+        );
+      }
+
+      /* 
+        A conta que NÃO aceita lançamento pode ser pai de outras contas,
+      */
       const newRecordParentId = newRecord.parentId;
       if (newRecordParentId) {
         const parent = recordsById.get(newRecordParentId);
-
-        /* 
-          A conta que NÃO aceita lançamento pode ser pai de outras contas,
-        */
         if (parent?.acceptRelease) {
           throw new Error("A conta que aceita lançamentos não pode ter filhas");
         }
